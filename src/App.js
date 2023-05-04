2d2afde978889924869b692fd3f974b4663ec21d
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-03 21:09:40 -0700

    error message in

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends React.Component {
       cityName:'',
       longitude:'',
       lattitude:'',
-      location: {}
+      location: {},
+      errorIn:null
     }
   }
 
@@ -24,36 +25,51 @@ class App extends React.Component {
 
   handleExplore = async (e) =>{
     e.preventDefault();
-    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format=json`
-    const response = await axios.get(url);
-    this.setState({
-      location: response.data[0],
-      displayInfo: true,
-      cityName: response.data[0].display_name,
-      longitude: response.data[0].lon,
-      lattitude:response.data[0].lat
-    });
-    console.log(response.data[0]);
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format=json`
+      const response = await axios.get(url);
+      this.setState({
+        location: response.data[0],
+        displayInfo: true,
+        cityName: response.data[0].display_name,
+        longitude: response.data[0].lon,
+        lattitude:response.data[0].lat
+      });
+      console.log(response.data[0]);
+    } catch (error){
+      this.setState({
+        errorIn: true
+      })
+      console.log(this.state.errorIn)
+    }
   }
   
   render (){
     return (
-     <Container>
+      <Container>
+      <h1 id='head'>The City Explorer</h1>
       <Form onSubmit={this.handleExplore}>
         <Form.Group className="mb-3">
           <Form.Label>Enter a city name</Form.Label>
           <Form.Control type='text' onChange={this.handleInput}></Form.Control>
         </Form.Group>
           <Button variant="primary" type='submit'>Explore!</Button>
-          </Form>
+      </Form>
+          
       {this.state.displayInfo &&
       <>
-      <p>{this.state.cityName} </p>
-      <p>The longitude of the city is: {this.state.longitude} </p>
-      <p>The lattitude of the city is: {this.state.lattitude} </p>
+      <ul id='cities'>{this.state.cityName} 
+      <li>The longitude of the city is: {this.state.longitude}</li>
+      <li>The lattitude of the city is: {this.state.lattitude}</li>
+      </ul>
      <Map lat={this.state.lattitude} lon={this.state.longitude}/>
       </>
       }
+        {this.state.errorIn  &&
+        <>
+        <p id='errorMessage'>Uh-Oh! Try again with the real city name</p>
+        </>
+        }
       </Container>
     )
   }
@@ -61,4 +77,3 @@ class App extends React.Component {
 }
 
 export default App;
-
---
+++ src/Error.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Error extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oops, something went wrong.</h1>;
+    }
+    return this.props.children; 
+  }
+}
+
+export default Error;
+
+
+
+
