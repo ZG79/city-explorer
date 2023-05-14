407d6df65789d95983e5a4ef873d17e6940aebec
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-13 18:30:15 -0700

    styling added

--- src/nestedComponent/WeatherDay.js
+++ src/nestedComponent/WeatherDay.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import '../map.css'; 
 
 class WeatherDay extends React.Component{
   render(){
     return(
       <>
+      <Card id="weather" style={{ width: '20rem', display: 'grid'}}>
       <p> {this.props.day.date}</p>
       <p> {this.props.day.description}</p>
       <p> The max temp is {this.props.day.highTemp}</p>
+      </Card>
       </>
     )
   }
