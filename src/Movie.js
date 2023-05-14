e12156d3c03596007c4fad77a9e78e52c844c45a
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-13 17:29:58 -0700

    movies nested component in

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,19 @@
 import React from "react";
 import './map.css';
 import Card from 'react-bootstrap/Card';
+import MovieOne from "./nestedComponent/MovieOne";
 
 class Movie extends React.Component{
   render(){
     return(
         <Card style={{ width: '18rem' }}>
-           
+
        {this.props.movie && 
          <Card.Text>
             {this.props.movie.map((element,idx)=> (
              <div key={idx}>
              <Card.Body> <p> Title: {element.name}</p> </Card.Body>
-              <p> Overview: {element.overview}</p>
-              <p> Popularity: {element.popularity}</p>
-              <p> Release Date: {element.release_date}</p>
+              <MovieOne clip={element}/>
               </div>
             ))}
 
