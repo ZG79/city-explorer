19cfeb24d677e24b42425c5e6f8dad8482052f88
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-15 20:38:23 -0700

    displayed icon

--- src/nestedComponent/WeatherDay.js
+++ src/nestedComponent/WeatherDay.js
@@ -3,12 +3,14 @@ import { Card } from "react-bootstrap";
 
 class WeatherDay extends React.Component{
   render(){
+    console.log(this.props);
     return(
       <>
       <Card id="weather" style={{ width: '21rem', display: 'grid'}}>
       <p> {this.props.day.date}</p>
       <p> {this.props.day.description}</p>
       <p> The max temp is {this.props.day.highTemp}</p>
+      <img alt="icon" src={`https://cdn.weatherbit.io/static/img/icons/${this.props.day.icon}.png`}/>
       </Card>
       </>
     )
@@ -16,3 +18,4 @@ class WeatherDay extends React.Component{
 }
 
 export default WeatherDay;
+// https://cdn.weatherbit.io/static/img/icons/${this.props.icon}.png
